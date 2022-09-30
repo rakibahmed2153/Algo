81ca35e60c0c5e09c41ee505ab562af9f2108212
Rakib Ahmed <36979611+rakibahmed2153@users.noreply.github.com>
2022-09-30 11:54:56 +0600

    DFS and BFS
    DFS and BFS

---
+++ DepthFirstSearch.py
@@ -0,0 +1,14 @@
+'''
+1. Visiting a vertex
+2. Exploration a vertes
+3. Pre order search
+4. Take a stack
+
+Steps:
+--------------------------------
+1. Set a vertex and explor the next vertex link to it.
+2. From that next vertex explore the next one and go to step 1 again.
+3. If the there is no vertex avaliabe then come back the previous vertex.
+
+
+'''
---
+++ breathFirstSearch.py
@@ -0,0 +1,12 @@
+'''
+1. Visiting a vertex
+2. Exploration a vertes
+3. Level order of a binary tree
+4. Take a queue
+
+Steps:
+--------------------------------
+1. Set a vertex and explor all the vertex link to it.
+2. Next set another vertex and explor all its vertexs.
+
+'''
